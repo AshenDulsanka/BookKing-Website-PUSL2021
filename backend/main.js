a0a3a390e0cd4bbc5790c58d6a05e31f2f33f6d7
Ashen Abeysekara <ashendul@gmail.com>
2024-03-06 21:32:40 +0530

    backend up n running

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,17 @@
-console.log('Hello World!')
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
