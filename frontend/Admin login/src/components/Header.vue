762f380f680d76882c85cc56aec0c177d3a9cc7f
rasithsenanayake <smrasithsenanayake@gmail.com>
2024-03-10 00:36:32 +0530

    added admin login

---
+++ frontend/Admin login/src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="login-container">
+    <h2>Login</h2>
+    <form @submit.prevent="handleSubmit">
+      <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username" required>
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleSubmit() {
+      // In a real app, you'd send an authentication request to your backend
+      console.log('Username:', this.username);
+      console.log('Password:', this.password); 
+    }
+  }
+}
+</script>
+
+<style>
+.login-container {
+  width: 300px;
+  margin: 30px auto;
+  padding: 20px;
+  border: 1px solid #ddd;
+}
+.form-group {
+  margin-bottom: 15px;
+}
+</style>
