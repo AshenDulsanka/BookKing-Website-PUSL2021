7ebb49be81c9c87a1563e1fa21c75baa10b78d8a
Ashen Abeysekara <ashendul@gmail.com>
2024-03-15 07:26:04 +0530

    added database connection

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,29 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import morgan from 'morgan'
+import dotenv from 'dotenv'
+import { conn } from './config/dbCon.js'
 
 const server = express()
 server.use(morgan('combined'))
 server.use(bodyParser.json())
 server.use(cors())
+dotenv.config()
 
 server.post('/userSignUp', (req, res) => {
   res.send({
     message: `Hello ${req.body.email}! Your user was registered! Have fun!`
+  })
+})
+
+console.log(conn.connect)
+
+// error handling
+server.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500
+  err.message = err.message || 'Internal Server Error'
+  res.status(err.statusCode).json({
+    message: err.message
   })
 })
 
