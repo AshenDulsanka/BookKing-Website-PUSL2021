a1f332a8ba60169de3977f676f37e0648c9b34b3
Ashen Abeysekara <ashendul@gmail.com>
2024-03-16 19:17:05 +0530

    verification email is sent

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,11 @@
 import { validationResult } from 'express-validator'
 import bcrypt from 'bcryptjs'
+import random from 'randomstring'
+import { sendMail } from '../helpers/sendMail.js'
 import { conn } from '../config/dbCon.js'
 
 const db = conn
+const randomToken = random.generate()
 
 const register = (req, res) => {
   const errors = validationResult(req)
@@ -31,16 +34,21 @@ const register = (req, res) => {
             })
           } else {
             db.query(
-              `INSERT INTO users (name, email, password, phoneNumber, address) VALUES ('${req.body.name}', ${db.escape(
+              `INSERT INTO users (name, email, password, phoneNumber, address, token) VALUES ('${req.body.name}', ${db.escape(
                 req.body.email
-              )}, ${db.escape(hash)}, '${req.body.phoneNo}', '${req.body.address}');`,
+              )}, ${db.escape(hash)}, '${req.body.phoneNo}', '${req.body.address}', '${randomToken}');`,
               (err, result) => {
                 if (err) {
                   return res.status(400).send({
                     msg: err
                   })
                 }
-                return res.status(500).send({
+                const mailSubject = 'Mail verification'
+                // eslint-disable-next-line no-multi-str
+                const content = '<p>Hi ' + req.body.name + ', \
+                Please verify your email by clicking on the <a href="http://localhost:8081/mailVerification?token=' + randomToken + '">link.</a>'
+                sendMail(req.body.email, mailSubject, content)
+                return res.status(200).send({
                   msg: 'The user has been registered successfully!'
                 })
               }
