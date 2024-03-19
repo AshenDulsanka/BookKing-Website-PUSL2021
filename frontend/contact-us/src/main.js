047bf8b8a0d16fb5b40d83bd2bf8e95033aeaf95
Minindu Gunawardana <dewruwanminindu@gmail.com>
2024-03-20 00:19:07 +0530

    Added contact us page

--- frontend/contact-us/src/main.js
+++ frontend/contact-us/src/main.js
@@ -1,6 +1,44 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import ContactUs from "./pages/ContactUs.vue";
+import "./global.css";
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: "/",
+    name: "ContactUs",
+    component: ContactUs,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((toRoute, fromRoute, next) => {
+  const documentTitle =
+    toRoute?.meta && toRoute?.meta?.title ? toRoute?.meta?.title : "ContactUs";
+  window.document.title = documentTitle;
+  if (toRoute?.meta?.description) {
+    addMetaTag(toRoute?.meta?.description);
+  }
+  next();
+});
+
+const addMetaTag = (value) => {
+  let element = document.querySelector(`meta[name='description']`);
+
+  if (element) {
+    element.setAttribute("content", value);
+  } else {
+    element = `<meta name="description" content="${value}" />`;
+    document.head.insertAdjacentHTML("beforeend", element);
+  }
+};
+
+createApp(App).use(router).mount("#app");
+
+export default router;
--- frontend/contact-us/src/pages/ContactUs.vue
+++ frontend/contact-us/src/pages/ContactUs.vue
@@ -1,217 +1,329 @@
 <template>
-    <div class="contact-us">
-      <div class="div-2">
-        <div class="text-wrapper-30">Customer Service - Contact</div>
-        <img class="rectangle" alt="Rectangle" src="rectangle-36.svg" />
-        <img class="rectangle-2" alt="Rectangle" src="rectangle-37.svg" />
-        <img class="rectangle-3" alt="Rectangle" src="rectangle-38.svg" />
-        <img class="rectangle-4" alt="Rectangle" src="rectangle-39.svg" />
-        <img class="rectangle-5" alt="Rectangle" src="rectangle-40.svg" />
-        <div class="text-wrapper-31">Name</div>
-        <div class="text-wrapper-32">Email Address</div>
-        <div class="phone-number">Phone&nbsp;&nbsp;Number</div>
-        <div class="text-wrapper-33">location</div>
-        <div class="text-wrapper-34">message</div>
-        <div class="text-wrapper-35">FAQ’s</div>
-        
-        <img class="line-2" alt="Line" src="line-4.svg" />
-        <img class="BOOK-king" alt="Book king" src="BOOK-king-1.png" />
-        <p class="p">
-          Connect with Us: Your Direct Line to Exceptional Service – Reach Out, Share Your Thoughts, and Let Us Assist
-          You!
-        </p>
-      </div>
+  <div class="contact-us">
+    <NewHeader />
+    <h1 class="customer-service-">Customer Service - Contact</h1>
+    <input class="input" type="text" />
+    <input class="input1" type="text" />
+    <input class="input2" type="text" />
+    <input class="input3" type="text" />
+    <input class="input4" type="text" />
+    <div class="name">Name</div>
+    <div class="email-address">Email Address</div>
+    <div class="phone-number">Phone Number</div>
+    <div class="location">Location</div>
+    <div class="message">Message</div>
+    <h1 class="faqs">FAQ’s</h1>
+    <div class="how-do-i-container">
+      <p class="how-do-i">How do I make a booking on your website?</p>
+      <ul class="to-make-a-booking-simply-visi">
+        <li>
+          <span class="to-make-a"
+            >To make a booking, simply visit our website and select the desired
+            service or product. Follow the prompts to enter relevant details,
+            choose your preferences, and complete the payment process.</span
+          >
+        </li>
+      </ul>
     </div>
-  </template>
-  
-  <script>
-  
-  
-  
-  export default {
+    <div class="can-i-modify-container">
+      <p class="how-do-i">Can I modify or cancel my reservation?</p>
+      <ul class="to-make-a-booking-simply-visi">
+        <li>
+          <span class="to-make-a"
+            >Yes, in most cases, you can modify or cancel your reservation
+            through your account on our website. Please refer to the specific
+            terms and conditions associated with your booking for more
+            information.</span
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="what-payment-methods-container">
+      <p class="how-do-i">What payment methods do you accept?</p>
+      <ul class="to-make-a-booking-simply-visi">
+        <li>
+          <span class="to-make-a"
+            >We accept a variety of payment methods, including credit/debit
+            cards, PayPal, and other secure online payment options. The
+            available methods will be displayed during the checkout
+            process.</span
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="is-my-payment-container">
+      <p class="how-do-i">Is my payment information secure on your website?</p>
+      <ul class="to-make-a-booking-simply-visi">
+        <li>
+          <span class="to-make-a"
+            >Yes, we take the security of your payment information seriously.
+            Our website uses encryption and follows industry best practices to
+            ensure your data is secure.</span
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="how-do-i-container1">
+      <p class="how-do-i">How do I retrieve my booking confirmation?</p>
+      <ul class="to-make-a-booking-simply-visi">
+        <li>
+          <span class="to-make-a"
+            >Once your booking is confirmed, you will receive a confirmation
+            email containing all the necessary details. You can also log in to
+            your account on our website to view and print your booking
+            confirmation.</span
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="contact-us-child" />
+    <img class="book-king-1" alt="" src="/book-king-1@2x.png" />
+    <section class="connect-with-us">
+      Connect with Us: Your Direct Line to Exceptional Service – Reach Out,
+      Share Your Thoughts, and Let Us Assist You!
+    </section>
+    <h1 class="contact-us1">Contact Us</h1>
+    <button class="button"><b>Submit</b></button>
+    <NewFooter />
+  </div>
+</template>
+
+<!--<script>
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
     name: "ContactUs",
-    components: {
-      Header,
-      Footer,
-      HowDoIMakeA,
-      CanIModifyOr,
-      WhatPaymentMethods,
-      IsMyPayment,
-      HowDoIRetrieveMy,
-    },
-  };
-  </script>
-  
-  <style>
-  .contact-us {
-    background-color: #ffffff;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    width: 100%;
-  }
-  
-  .contact-us .div-2 {
-    background-color: #ffffff;
-    height: 3748px;
-    overflow: hidden;
-    position: relative;
-    width: 1920px;
-  }
-  
-  .contact-us .text-wrapper-30 {
-    color: #000000;
-    font-family: "Poppins-Bold", Helvetica;
-    font-size: 32px;
+  });
+</script>-->
+
+<script>
+  import { defineComponent } from "vue";
+  import NewHeader from "/src/components/newheader.vue";
+  import NewFooter from "/src/components/newfooter.vue";
+
+  export default defineComponent({
+    name: "ContactUs",
+    components: { NewHeader, NewFooter },
+  });
+</script>
+
+<style scoped>
+  .customer-service- {
+    margin: 0;
+    position: absolute;
+    top: 350px;
+    left: 162px;
+    font-size: var(--font-size-13xl);
+    letter-spacing: 0.09em;
     font-weight: 700;
-    left: 99px;
-    letter-spacing: 2.88px;
-    line-height: normal;
-    position: absolute;
-    top: 535px;
+    font-family: inherit;
   }
-  
-  .contact-us .rectangle {
-    height: 48px;
-    left: 99px;
+  .input {
+    border: none;
+    outline: none;
+    background-color: var(--color-khaki);
     position: absolute;
-    top: 796px;
+    top: 610px;
+    left: 162px;
     width: 807px;
-  }
-  
-  .contact-us .rectangle-2 {
     height: 48px;
-    left: 99px;
-    position: absolute;
-    top: 911px;
-    width: 807px;
   }
-  
-  .contact-us .rectangle-3 {
+  .input1 {
+    border: none;
+    outline: none;
+    background-color: var(--color-khaki);
+    position: absolute;
+    top: 725px;
+    left: 162px;
+    width: 807px;
     height: 48px;
-    left: 99px;
-    position: absolute;
-    top: 1026px;
-    width: 807px;
   }
-  
-  .contact-us .rectangle-4 {
+  .input2 {
+    border: none;
+    outline: none;
+    background-color: var(--color-khaki);
+    position: absolute;
+    top: 840px;
+    left: 162px;
+    width: 807px;
     height: 48px;
-    left: 99px;
-    position: absolute;
-    top: 1141px;
-    width: 807px;
   }
-  
-  .contact-us .rectangle-5 {
+  .input3 {
+    border: none;
+    outline: none;
+    background-color: var(--color-khaki);
+    position: absolute;
+    top: 955px;
+    left: 162px;
+    width: 807px;
+    height: 48px;
+  }
+  .input4 {
+    border: none;
+    outline: none;
+    background-color: var(--color-khaki);
+    position: absolute;
+    top: 1070px;
+    left: 162px;
+    width: 807px;
     height: 256px;
-    left: 99px;
-    position: absolute;
-    top: 1256px;
-    width: 807px;
   }
-  
-  .contact-us .text-wrapper-31 {
-    color: #000000;
-    font-family: "Poppins-SemiBold", Helvetica;
-    font-size: 16px;
-    font-weight: 600;
-    left: 99px;
-    letter-spacing: 0;
-    line-height: normal;
+  .name {
     position: absolute;
-    top: 765px;
-  }
-  
-  .contact-us .text-wrapper-32 {
-    color: #000000;
-    font-family: "Poppins-SemiBold", Helvetica;
-    font-size: 16px;
+    top: 580px;
+    left: 162px;
     font-weight: 600;
-    left: 99px;
-    letter-spacing: 0;
-    line-height: normal;
+  }
+  .email-address {
     position: absolute;
-    top: 880px;
-  }
-  
-  .contact-us .phone-number {
-    color: #000000;
-    font-family: "Poppins-SemiBold", Helvetica;
-    font-size: 16px;
+    top: 695px;
+    left: 162px;
     font-weight: 600;
-    left: 99px;
-    letter-spacing: 0;
-    line-height: normal;
+  }
+  .phone-number {
     position: absolute;
-    top: 995px;
-  }
-  
-  .contact-us .text-wrapper-33 {
-    color: #000000;
-    font-family: "Poppins-SemiBold", Helvetica;
-    font-size: 16px;
+    top: 810px;
+    left: 162px;
     font-weight: 600;
-    left: 99px;
-    letter-spacing: 0;
-    line-height: normal;
+    white-space: pre-wrap;
+  }
+  .location {
     position: absolute;
-    top: 1110px;
-  }
-  
-  .contact-us .text-wrapper-34 {
-    color: #000000;
-    font-family: "Poppins-SemiBold", Helvetica;
-    font-size: 16px;
+    top: 925px;
+    left: 162px;
     font-weight: 600;
-    left: 99px;
-    letter-spacing: 0;
-    line-height: normal;
+  }
+  .message {
     position: absolute;
-    top: 1225px;
-  }
-  
-  .contact-us .text-wrapper-35 {
-    color: #000000;
-    font-family: "Poppins-SemiBold", Helvetica;
-    font-size: 32px;
+    top: 1040px;
+    left: 162px;
     font-weight: 600;
+  }
+  .faqs {
+    margin: 0;
+    position: absolute;
+    top: 1452px;
+    left: 121px;
+    font-size: var(--font-size-21xl);
+    font-weight: 600;
+    font-family: inherit;
+  }
+  .how-do-i {
+    margin: 0;
+    font-weight: 600;
+  }
+  .to-make-a {
+    font-weight: 300;
+  }
+  .to-make-a-booking-simply-visi {
+    margin: 0;
+    font-size: inherit;
+    padding-left: var(--padding-lgi);
+  }
+  .how-do-i-container {
+    position: absolute;
+    top: 1586px;
     left: 123px;
-    letter-spacing: 0;
-    line-height: normal;
-    position: absolute;
-    top: 1782px;
+    font-size: var(--font-size-sm);
+    display: inline-block;
+    width: 1679px;
   }
-  
-  .contact-us .line-2 {
-    height: 1px;
+  .can-i-modify-container {
+    position: absolute;
+    top: 1680px;
+    left: 121px;
+    font-size: var(--font-size-sm);
+    display: inline-block;
+    width: 1679px;
+  }
+  .what-payment-methods-container {
+    position: absolute;
+    top: 1774px;
     left: 118px;
-    object-fit: cover;
-    position: absolute;
-    top: 1860px;
-    width: 1655px;
+    font-size: var(--font-size-sm);
+    display: inline-block;
+    width: 1679px;
   }
-  
-  .contact-us .BOOK-king {
-    height: 500px;
-    left: 1179px;
-    object-fit: cover;
+  .is-my-payment-container {
     position: absolute;
-    top: 861px;
+    top: 1868px;
+    left: 118px;
+    font-size: var(--font-size-sm);
+    display: inline-block;
+    width: 1679px;
+  }
+  .how-do-i-container1 {
+    position: absolute;
+    top: 1962px;
+    left: 121px;
+    font-size: var(--font-size-sm);
+    display: inline-block;
+    width: 1679px;
+  }
+  .contact-us-child {
+    position: absolute;
+    top: 1530.5px;
+    left: 117.5px;
+    border-top: 1px solid var(--color-black);
+    box-sizing: border-box;
+    width: 1656px;
+    height: 1px;
+  }
+  .book-king-1 {
+    position: absolute;
+    top: 687px;
+    left: 1242px;
     width: 500px;
+    height: 500px;
+    object-fit: cover;
   }
-  
-  .contact-us .p {
-    color: #000000;
-    font-family: "Poppins-Regular", Helvetica;
-    font-size: 32px;
-    font-weight: 400;
-    left: 99px;
-    letter-spacing: 0;
-    line-height: normal;
+  .connect-with-us {
     position: absolute;
-    top: 583px;
+    top: 398px;
+    left: 162px;
+    font-size: var(--font-size-13xl);
+    font-family: var(--font-poppins);
+    color: var(--color-black);
+    text-align: left;
+    display: inline-block;
     width: 1597px;
+    height: 95px;
   }
-  </style>
-  
+  .contact-us1 {
+    margin: 0;
+    position: absolute;
+    top: 216px;
+    left: 123px;
+    font-size: var(--font-size-21xl);
+    font-weight: 700;
+    font-family: var(--font-tajawal);
+    display: inline-block;
+    width: 236px;
+    height: 39px;
+  }
+
+  .button {
+    cursor: pointer;
+    border: none;
+    padding: 0;
+    background-color: transparent;
+    position: absolute;
+    top: 1363px;
+    left: 171px;
+    width: 99px;
+    height: 36px;
+    border-radius: 4px;
+    background-color: #faff00;
+  }
+  .contact-us {
+    width: 100%;
+    height: 2281px;
+    position: relative;
+    background-color: #fff;
+    overflow: hidden;
+    text-align: left;
+    font-size: var(--font-size-base);
+    color: var(--color-black);
+    font-family: var(--font-poppins);
+  }
+</style>
