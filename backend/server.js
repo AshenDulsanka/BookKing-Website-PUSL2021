9abf4a978709970105ae3944a0aae344a455c9ed
Ashen <ashendul@gmail.com>
2024-03-20 16:05:13 +0530

    changed backend port to 3000

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ server.use((err, req, res, next) => {
   })
 })
 
-const PORT = process.env.PORT || 8081
+const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
