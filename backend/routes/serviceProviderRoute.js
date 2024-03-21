68e4f06dbbbf12621987e99344e0c6219bc30553
Ashen Abeysekara <ashendul@gmail.com>
2024-03-21 05:52:03 +0530

    added sp controller and router. and changed page title to bookking

---
+++ backend/routes/serviceProviderRoute.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { userSignUpValidation, logInValidation, forgetValidation, updateProfileValidation } from '../helpers/validation.js'
+import { register, login, getUser, forgetPassword, updateProfile } from '../controllers/userController.js'
+import { isAuthorized } from '../middleware/auth.js'
+
+const userRouter = express.Router()
+
+userRouter.post('/register', userSignUpValidation, register)
+userRouter.post('/login', logInValidation, login)
+
+userRouter.get('/getUser', isAuthorized, getUser)
+
+userRouter.post('/forgetPassword', forgetValidation, forgetPassword)
+
+userRouter.post('/updateProfile', updateProfileValidation, isAuthorized, updateProfile)
+
+export { userRouter }
