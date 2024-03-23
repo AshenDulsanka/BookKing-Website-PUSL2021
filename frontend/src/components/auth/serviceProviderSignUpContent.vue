25ced9f8078e28079e3f444e4cbfdc0e784d5947
Ashen Abeysekara <ashendul@gmail.com>
2024-03-24 02:45:41 +0530

    connected sp register page to the backend

--- frontend/src/components/auth/serviceProviderSignUpContent.vue
+++ frontend/src/components/auth/serviceProviderSignUpContent.vue
@@ -7,35 +7,80 @@
       <div class="email-parent">
         <br>
         <div class="email1">Name</div>
-        <input class="rectangle-rectangle" type="text" name="name" required/>
+        <input class="rectangle-rectangle" type="text" name="name" required v-model="name"/>
         <div class="email1">Email</div>
-        <input class="frame-item" type="email" name="email" required/>
+        <input class="frame-item" type="email" name="email" required v-model="email"/>
         <div class="email1">Password</div>
-        <input class="frame-item" type="password" name="password" required/>
+        <input class="frame-item" type="password" name="password" required v-model="password"/>
         <div class="email1">Password Confirm</div>
-        <input class="frame-item" type="password" name="confirmPassword" required/>
+        <input class="frame-item" type="password" name="confirmPassword" required v-model="confirmPassword"/>
         <div class="email1">Phone Number</div>
-        <input class="frame-item" type="text" name="phoneno" required/>
+        <input class="frame-item" type="text" name="phoneno" required v-model="phoneNo"/>
         <div class="email1">Address</div>
-        <input class="frame-item" type="text" name="address" required/>
+        <input class="frame-item" type="text" name="address" required v-model="address"/>
         <div class="email1">What we do</div>
-        <textarea class="frame-item1" name="whatwedo" rows="4" cols="50" required></textarea>
+        <textarea class="frame-item1" name="whatwedo" rows="4" cols="50" required v-model="serviceDesc"></textarea>
       </div>
     </div>
-    <a href="#">
-        <button class="ok">
-            <div class="ok">Register</div>
-        </button>
-    </a>
+    <button class="ok" @click="submitForm">Register</button>
+    <div v-if="error" class="error-message">{{ error }}</div>
   </div>
 </template>
+
 <script>
 import { defineComponent } from 'vue'
+import axios from 'axios'
 
 export default defineComponent({
-  name: 'serviceProviderSignUpContent'
+  name: 'serviceProviderSignUpContent',
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      phoneNo: '',
+      address: '',
+      serviceDesc: ''
+    }
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const response = await fetch('http://localhost:8081/api/spregister', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: this.name,
+            email: this.email,
+            password: this.password,
+            confirmPassword: this.confirmPassword,
+            phoneNo: this.phoneNo,
+            address: this.address,
+            serviceDesc: this.serviceDesc
+          }),
+        });
+
+        const responseData = await response.json();
+        
+        if (response.ok) {
+          this.$emit('login-success', true);
+          alert('Registration successful, please verify your email.');
+          this.$router.push('/');
+        } else {
+          this.error = alert('Passwords do not match');
+        }
+      } catch (error) {
+        console.error('Error occurred:', error);
+        this.error = 'An error occurred. Please try again later.';
+      }
+    }
+  }
 })
 </script>
+
 <style scoped>
   .frame-item1{
     border: none;
