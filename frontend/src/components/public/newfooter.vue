48771cdf0965ae5a969684aae35b3af1bd0dbf3c
Ashen Abeysekara <ashendul@gmail.com>
2024-03-25 05:48:03 +0530

    footer needs to be fixed

--- frontend/src/components/public/newfooter.vue
+++ frontend/src/components/public/newfooter.vue
@@ -2,63 +2,63 @@
   <footer>
     <nav>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/vehicles">Vehicles</a></li>
-        <li><a href="/hotels">Hotels</a></li>
-        <li><a href="/tours">Tours</a></li>
-        <li><a href="/reviews">Reviews</a></li>
-        <li><a href="/contactus">Contact us</a></li>
-        <li><a href="/aboutus">About us</a></li>
-        <li>
+        <li class="hideOnMobile"><a href="/">Home</a></li>
+        <li class="hideOnMobile"><a href="/vehicles">Vehicles</a></li>
+        <li class="hideOnMobile"><a href="/hotels">Hotels</a></li>
+        <li class="hideOnMobile"><a href="/tours">Tours</a></li>
+        <li class="hideOnMobile"><a href="/reviews">Reviews</a></li>
+        <li class="hideOnMobile"><a href="/contactus">Contact us</a></li>
+        <li class="hideOnMobile"><a href="/aboutus">About us</a></li>
+        <li class="hideOnMobile">
           <a v-if="!isLoggedIn" class="login" href="/login">Login</a>
           <a v-else href="#" @click.prevent="redirectToDashboard">Profile</a>
         </li>
-        <li v-if="isLoggedIn"><a href="#" @click.prevent="logout">Logout</a></li>
+        <li v-if="isLoggedIn" class="hideOnMobile"><a href="#" @click.prevent="logout">Logout</a></li>
       </ul>
     </nav>
   </footer>
 </template>
 
 <script>
-import { jwtDecode } from 'jwt-decode';
+  import { jwtDecode } from 'jwt-decode';
 
-export default {
-  name: 'ResponsiveFooter',
-  data() {
-    return {
-      userRole: null,
-    };
-  },
-  computed: {
-    isLoggedIn() {
+  export default {
+    name: 'ResponsiveFooter',
+    data() {
+      return {
+        userRole: null,
+      };
+    },
+    computed: {
+      isLoggedIn() {
+        const token = localStorage.getItem('token');
+        return !!token; 
+      },
+    },
+    created() {
       const token = localStorage.getItem('token');
-      return !!token;
-    },
-  },
-  created() {
-    const token = localStorage.getItem('token');
-    if (token) {
-      const decodedToken = jwtDecode(token);
-      this.userRole = decodedToken.role;
-    }
-  },
-  methods: {
-    logout() {
-      localStorage.removeItem('token');
-      this.userRole = null;
-      this.$router.push('/login');
-    },
-    redirectToDashboard() {
-      if (this.userRole === 'user') {
-        this.$router.push('/userdashboard');
-      } else if (this.userRole === 'serviceProvider') {
-        this.$router.push('/spdashboard');
-      } else if (this.userRole === 'admin') {
-        this.$router.push('/admindashboard');
+      if (token) {
+        const decodedToken = jwtDecode(token);
+        this.userRole = decodedToken.role;
       }
     },
-  },
-};
+    methods: {
+      logout() {
+        localStorage.removeItem('token');
+        this.userRole = null;
+        this.$router.push('/login');
+      },
+      redirectToDashboard() {
+        if (this.userRole === 'user') {
+          this.$router.push('/userdashboard');
+        } else if (this.userRole === 'serviceProvider') {
+          this.$router.push('/spdashboard');
+        } else if (this.userRole === 'admin') {
+          this.$router.push('/admindashboard');
+        }
+      },
+    },
+  };
 </script>
 
 <style scoped>
@@ -67,29 +67,25 @@ footer {
   height: 70px;
   bottom: 0;
 }
-
 nav {
   background-color: rgb(0, 0, 0);
   box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.1);
   height: 70px;
   padding-top: 10px;
 }
-
 nav ul {
   width: 100%;
   list-style: none;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
   font-size: 14px;
   font-weight: 500;
 }
-
 nav li {
   height: 50px;
 }
-
 nav a {
   height: 100%;
   padding: 0 30px;
@@ -98,16 +94,13 @@ nav a {
   align-items: center;
   color: white;
 }
-
 nav a:hover {
   color: black;
   font-weight: 700;
 }
-
 .hideOnMobile:hover {
   background-color: yellow;
 }
-
 .login {
   font-weight: bold;
 }
@@ -115,19 +108,20 @@ nav a:hover {
 @media (max-width: 1200px) {
   nav ul {
     justify-content: center;
-    display: flex;
-    flex-wrap: wrap;
-    padding: 0;
-    margin-top: 0;
+    display:block;
+    padding:0px;
+    margin-top: 100px;
   }
-
   nav li {
-    width: auto;
+    width: 100%;
     text-align: center;
+    
   }
-
   .nav {
-    background-color: transparent;
+  background-color:transparent;
+  }
+  .hideOnMobile{
+    display: none;
   }
 }
 
